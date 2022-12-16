09c887795af419e187cdf9707d523f17bc6baee2
caioguarnieri <92325326+caioguarnieri@users.noreply.github.com>
2022-12-16 11:55:54 +0000

    adiciona a rota para criar um livro

---
+++ routes/book.routes.js
@@ -0,0 +1,70 @@
+const router = require("express").Router();
+
+const BookModel = require("../models/Book.model");
+const isAuthenticated = require("../middlewares/isAuthenticated");
+const attachCurrentUser = require("../middlewares/attachCurrentUser");
+const isAdmin = require("../middlewares/isAdmin");
+
+// Criar um novo livro
+router.post(
+    "/book",
+    isAuthenticated,
+    attachCurrentUser,
+    isAdmin,
+    async (req, res) => {
+      try {
+        const { author, title, synopsis, releaseYear, genre, coverImage } =
+          req.body;
+  
+        // Verifica se o author não está em branco
+        if (!author || author.trim().length === 0) {
+          return res.status(400).json({
+            msg: "Author is required",
+          });
+        }
+  
+        // Verifica se o título não está em branco
+        if (!title || title.trim().length === 0) {
+          return res.status(400).json({
+            msg: "Title is required",
+          });
+        }
+  
+        // Verifica se o ano de lançamento não está em branco ou se não é do tipo permitido
+        if (!releaseYear || !Number(releaseYear)) {
+          return res.status(400).json({
+            msg: "ReleaseYear is required and must be of number",
+          });
+        }
+  
+        // Veficia se o id (author) enviado é compatível com o modelo de id do mongodb
+        if (!author.match(/^[0-9a-fA-F]{24}$/)) {
+          return res.status(404).json({ error: "Author not found" });
+        }
+  
+        const loggedInUser = req.currentUser;
+  
+        // Verifica se o id do author enviado por parâmetro é o mesmo que o usuário logado identificado pelo token enviado pela requisição
+        if (!loggedInUser || loggedInUser._id.toString() !== author) {
+          return res.status(404).json({
+            msg: "Only the logged in user can create a new book",
+          });
+        }
+  
+        // Salva os dados de livro no banco de dados
+        const book = await BookModel.create({
+          author,
+          title,
+          synopsis,
+          releaseYear,
+          genre,
+          coverImage,
+        });
+  
+        return res.status(201).json(book);
+      } catch (err) {
+        console.error(err);
+        return res.status(500).json({ msg: JSON.stringify(err) });
+      }
+    }
+  );
