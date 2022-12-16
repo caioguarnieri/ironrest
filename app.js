f7384a324627024beddf2ff55ce1b178511086ff
caioguarnieri <92325326+caioguarnieri@users.noreply.github.com>
2022-12-16 12:06:10 +0000

    adiciona as rotas de livro ao app.js

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 require("dotenv").config();
+require("express-async-errors");
 const express = require("express");
 const cors = require("cors");
 const morgan = require("morgan");
 require("./config/db.config")();
+
+const handleErrors = require("./middlewares/handleErrors");
 
 const app = express();
 
@@ -12,7 +15,12 @@ app.use(morgan("dev"));
 app.use(cors({ origin: process.env.REACT_APP_URL }));
 
 const userRouter = require("./routes/user.routes");
+const bookRouter = require("./routes/book.routes");
 app.use("/api", userRouter);
+app.use("/api", bookRouter);
+
+// Middleware que intercepta os erros gerados no fileFilter do multer
+app.use(handleErrors);
 
 app.listen(Number(process.env.PORT), () =>
   console.log(`Server up and running at port ${process.env.PORT}`)
